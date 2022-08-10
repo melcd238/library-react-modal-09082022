40e6096e709e1e214f8050dc5d8f2188f6982c2f
melcd238 <melcd238@gmail.com>
2022-08-10 13:45:17 +0200

    update style an props to Modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import Modal from './lib/component/modal';
+import React, {useState } from 'react';
+import ReactDOM from 'react-dom'
 
 
 function App() {
-  return (
-   <Modal/>
-  );
+
+  const [isShowing, setIsShowing] = useState(true)
+
+  const closeModal = () =>{
+    setIsShowing(!isShowing)
+  }
+
+
+  return isShowing ? ReactDOM.createPortal(
+    <>
+       <Modal label="Mon texte" closeModal={(e)=>closeModal()}/>
+    </>, document.body
+  )
+  :
+  null
 }
 
 export default App;
--- src/lib/component/modal.js
+++ src/lib/component/modal.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './modal.css'
 
-const Modal = (props)=>{
+const Modal = ({closeModal,label,stylesForModal})=>{
+    
+
     return(
-        <>hello world</>
+        <div className='modal-container'>
+                <div className='modal'>
+                   <div className='modal-header'>
+                        <button className='btn-close-modal' onClick={closeModal}>X</button>
+                   </div>
+                   <div className='modal-content'>
+                        <p>{label}</p>
+                   </div>
+                </div>
+        </div>
     )
 }
 
